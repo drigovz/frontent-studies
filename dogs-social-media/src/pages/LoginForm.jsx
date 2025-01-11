e223af4d39193c34b48ae77c538a55cc102fff5e
Rodrigo Vaz <rodrigodp2014@gmail.com>
2025-01-11 14:40:00 -0300

    feat: add hook api endpoints to login

--- dogs-social-media/src/pages/LoginForm.jsx
+++ dogs-social-media/src/pages/LoginForm.jsx
@@ -1,38 +1,65 @@
 import { Link } from 'react-router-dom';
 import Input from '../components/Input';
 import useForm from '../hooks/useForm';
-import { validationType } from '../utilities/enums';
+// import { validationType } from '../utilities/enums';
+import { TOKEN_POST } from '../api/tokenEndpoints';
+import { USER_GET } from '../api/userEndpoints';
+import { useEffect } from 'react';
 
 const LoginForm = () => {
   // passando o tipo de validação para o hook useForm
-  const username = useForm(validationType.email);
-  const password = useForm(validationType.password);
+  // const username = useForm(validationType.email);
+  // const password = useForm(validationType.password);
+  const username = useForm();
+  const password = useForm();
+
+  function saveToken(token) {
+    window.localStorage.setItem('_t', token);
+  }
+
+  async function getUser(token) {
+    const { url, options } = USER_GET(token);
+    try {
+      const response = await fetch(url, options);
+      if (!response.ok) {
+        throw new Error('Error: User fetch error');
+      }
+
+      const json = await response.json();
+      console.log(json);
+    } catch (error) {
+      throw new Error('Error fetching user:', error.message);
+    }
+  }
+
+  // try get user when access the page
+  useEffect(() => {
+    const token = window.localStorage.getItem('_t');
+    if (token) {
+      getUser(token);
+    }
+  }, []);
 
   async function handleSubmit(event) {
     event.preventDefault();
 
     // realizamos as validações nos campos antes do fetch para a API de login
     if (username.validate() && password.validate()) {
-      try {
-        const url = 'https://dogsapi.origamid.dev/json/jwt-auth/v1/token';
-        const config = {
-          method: 'POST',
-          headers: {
-            Accept: 'application/json',
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({
-            username: username.value,
-            password: password.value,
-          }),
-        };
+      const { url, options } = TOKEN_POST({ username: username.value, password: password.value });
 
-        const response = await fetch(url, config);
+      try {
+        const response = await fetch(url, options);
         if (!response.ok) {
           throw new Error('Error: Login error');
         }
 
-        const loginData = await response.json();
+        const json = await response.json();
+        // save token on localstorage
+        saveToken(json.token);
+        getUser(json.token);
+        // clean inputs before login success
+        username.setValue('');
+        password.setValue('');
       } catch (error) {
         throw new Error('Error fetching login:', error.message);
       }
