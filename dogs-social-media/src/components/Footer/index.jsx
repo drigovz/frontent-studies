72bcd38976a9cf7c11ca323da0754a7b5585a5d9
Rodrigo Vaz <rodrigodp2014@gmail.com>
2025-03-04 10:44:49 -0300

    feat: add footer

--- dogs-social-media/src/components/Footer/index.jsx
+++ dogs-social-media/src/components/Footer/index.jsx
@@ -1,7 +1,18 @@
+import Dogs from '../../assets/dogs-footer.svg?react';
 import './styles.css';
 
 const Footer = () => {
-  return <></>;
+  const year = new Date().getFullYear();
+
+  return (
+    <footer className="footer">
+      <Dogs />
+      <p>Dogs. All rights reserved.</p>
+      <p>
+        <b>{year}</b>
+      </p>
+    </footer>
+  );
 };
 
 export default Footer;
