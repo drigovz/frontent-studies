60086921b7c28e2b9c462e9794d368b2c625b4af
Rodrigo Vaz <rodrigodp2014@gmail.com>
2025-04-19 20:26:51 -0300

    feat: print data on table

--- typescript-dom/src/js/script.js
+++ typescript-dom/src/js/script.js
@@ -1,14 +1,28 @@
 import fetchData from "./api/fetchData.js";
 import { url } from "./utils/conts.js";
 import normalizedInterface from "./utils/utilities.js";
+function printTransactions(transactions) {
+    const table = document.querySelector("#transactions tbody");
+    if (!table)
+        return;
+    transactions.forEach((transaction) => {
+        table.innerHTML += `
+      <tr>
+        <td>${transaction.name}</td>
+        <td>${transaction.email}</td>
+        <td>R$ ${transaction.amount}</td>
+        <td>${transaction.paymentType}</td>
+        <td>${transaction.status}</td>
+      </tr>
+    `;
+    });
+}
 async function handleData() {
     const data = await fetchData(url);
     if (!data)
         return;
     const transactions = data.map(normalizedInterface);
-    transactions.forEach((item) => {
-        console.log(item);
-    });
+    printTransactions(transactions);
 }
 handleData();
 //# sourceMappingURL=script.js.map
--- typescript-dom/src/ts/script.ts
+++ typescript-dom/src/ts/script.ts
@@ -2,15 +2,31 @@ import fetchData from "./api/fetchData.js";
 import { url } from "./utils/conts.js";
 import TransactionApi from "./interfaces/TransactionApi.js";
 import normalizedInterface from "./utils/utilities.js";
+import Transaction from "./interfaces/Transaction.js";
+
+function printTransactions(transactions: Transaction[]): void {
+  const table = document.querySelector("#transactions tbody");
+  if (!table) return;
+
+  transactions.forEach((transaction) => {
+    table.innerHTML += `
+      <tr>
+        <td>${transaction.name}</td>
+        <td>${transaction.email}</td>
+        <td>R$ ${transaction.amount}</td>
+        <td>${transaction.paymentType}</td>
+        <td>${transaction.status}</td>
+      </tr>
+    `;
+  });
+}
 
 async function handleData() {
   const data = await fetchData<TransactionApi[]>(url);
   if (!data) return;
 
   const transactions = data.map(normalizedInterface);
-  transactions.forEach((item) => {
-    console.log(item);
-  });
+  printTransactions(transactions);
 }
 
 handleData();
