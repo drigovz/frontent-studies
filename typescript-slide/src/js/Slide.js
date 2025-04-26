fb83050f4d9b6a953b28bf92702318d7eed227ba
Rodrigo Vaz <rodrigodp2014@gmail.com>
2025-04-26 18:38:49 -0300

    feat: add properties and methods to manage slide elements

--- typescript-slide/src/js/Slide.js
+++ typescript-slide/src/js/Slide.js
@@ -1,13 +1,33 @@
 export default class Slide {
     container;
-    elements;
+    slides;
     controls;
     time;
-    constructor(container, elements, controls, time = 5000) {
+    index;
+    slideActive;
+    constructor(container, slides, controls, time = 5000) {
         this.container = container;
-        this.elements = elements;
+        this.slides = slides;
         this.controls = controls;
         this.time = time;
+        // inicia o índice e o slide atualmente ativo como o zero
+        this.index = 0;
+        this.slideActive = this.slides[this.index];
+        this.show(this.index);
+    }
+    hide(slide) {
+        slide.classList.remove("active");
+    }
+    // método que exibe o slide
+    show(index) {
+        // qual o índice do slide que está ativo no momento
+        this.index = index;
+        // qual o slide que está ativo no momento
+        this.slideActive = this.slides[index];
+        // primeiro removemos a class active de todos os slides
+        this.slides.forEach((slide) => this.hide(slide));
+        // depois ativamos somente no elemento cujo index foi passado por parametro
+        this.slideActive.classList.add("active");
     }
 }
 //# sourceMappingURL=Slide.js.map
--- typescript-slide/src/ts/Slide.ts
+++ typescript-slide/src/ts/Slide.ts
@@ -1,18 +1,42 @@
 export default class Slide {
   private container: Element;
-  private elements: Element[];
+  private slides: Element[];
   private controls: Element;
   private time: number;
+  private index: number;
+  public slideActive: Element;
 
   constructor(
     container: Element,
-    elements: Element[],
+    slides: Element[],
     controls: Element,
     time: number = 5000
   ) {
     this.container = container;
-    this.elements = elements;
+    this.slides = slides;
     this.controls = controls;
     this.time = time;
+
+    // inicia o índice e o slide atualmente ativo como o zero
+    this.index = 0;
+    this.slideActive = this.slides[this.index];
+
+    this.show(this.index);
+  }
+
+  private hide(slide: Element) {
+    slide.classList.remove("active");
+  }
+
+  // método que exibe o slide
+  show(index: number) {
+    // qual o índice do slide que está ativo no momento
+    this.index = index;
+    // qual o slide que está ativo no momento
+    this.slideActive = this.slides[index];
+    // primeiro removemos a class active de todos os slides
+    this.slides.forEach((slide) => this.hide(slide));
+    // depois ativamos somente no elemento cujo index foi passado por parametro
+    this.slideActive.classList.add("active");
   }
 }
