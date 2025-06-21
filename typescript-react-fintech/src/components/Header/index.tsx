bc1ccb21e1e751e732942a2dbc8ef44f40021c1c
Rodrigo Vaz <rodrigodp2014@gmail.com>
2025-06-21 16:41:13 -0300

    feat: update sidenav page

--- typescript-react-fintech/src/components/Header/index.tsx
+++ typescript-react-fintech/src/components/Header/index.tsx
@@ -1,12 +1,16 @@
+import { useState } from 'react';
 import DateRage from '../DateRange';
 import Months from '../Months';
 
 const Header = () => {
+  const [title, setTitle] = useState('Summary');
+
   return (
     <>
       <header className="mb">
-        <div className="mb">
+        <div className="daterange mb">
           <DateRage />
+          <h1 className="box bg-three">{title}</h1>
         </div>
 
         <Months />
