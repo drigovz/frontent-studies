8162d57332bd6ee4ef3e6cd7943f805a667a4ce0
Rodrigo Vaz <rodrigodp2014@gmail.com>
2025-07-08 21:07:07 -0300

    test: clear project and remove jest

--- typescript-react-fintech/jest.config.ts
+++
@@ -1,9 +0,0 @@
-export default {
-  testEnvironment: 'jest-environment-jsdom',
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
-  moduleNameMapper: {
-    '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/test/__mocks__/fileMock.ts',
-    '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
-    '^@/(.*)$': '<rootDir>/src/$1',
-  },
-};
